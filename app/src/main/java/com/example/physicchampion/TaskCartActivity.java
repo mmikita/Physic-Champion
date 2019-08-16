3a09aeb0a627f9c05c425f38e9e10a21f09bda2c
Michał Mikita <paster01@gmail.com>
2019-08-16 19:29:09 +0200

    add linear layout to task view

--- app/src/main/java/com/example/physicchampion/TaskCartActivity.java
+++ app/src/main/java/com/example/physicchampion/TaskCartActivity.java
@@ -1,8 +1,10 @@
 package com.example.physicchampion;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class TaskCartActivity extends Activity {
 
@@ -10,5 +12,13 @@ public class TaskCartActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_cart);
+        TextView tx = (TextView)findViewById(R.id.task);
+        Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/sciencefair.otf");
+        tx.setTypeface(custom_font);
+
+
+
+
+
     }
 }
