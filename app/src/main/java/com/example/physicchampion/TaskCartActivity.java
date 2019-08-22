c151a96a0971e02fd8544ac09957317cc407e095
Michał Mikita <paster01@gmail.com>
2019-08-22 21:16:25 +0200

    add 3 icons

--- app/src/main/java/com/example/physicchampion/TaskCartActivity.java
+++ app/src/main/java/com/example/physicchampion/TaskCartActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class TaskCartActivity extends Activity {
@@ -16,6 +17,14 @@ public class TaskCartActivity extends Activity {
         Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/sciencefair.otf");
         tx.setTypeface(custom_font);
 
+        ImageButton ib1 = (ImageButton)findViewById(R.id.Ikonka1);
+        ib1.setImageResource(R.drawable.whiteflag);
+
+        ImageButton ib2 = (ImageButton)findViewById(R.id.Ikonka2);
+        ib2.setImageResource(R.drawable.texticon);
+
+        ImageButton ib3 = (ImageButton)findViewById(R.id.Ikonka3);
+        ib3.setImageResource(R.drawable.playicon);
 
 
 
